2b49f24a816f6396440924f3f2672fd1abdae080
XSectorz <panatschool@gmail.com>
2024-01-25 18:53:50 +0700

    Init

---
+++ XSGuildBungee/src/main/java/net/xsapi/panat/xsguildbungee/core.java
@@ -0,0 +1,16 @@
+package net.xsapi.panat.xsguildbungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class core extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[PLUGIN] ENABLED!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
