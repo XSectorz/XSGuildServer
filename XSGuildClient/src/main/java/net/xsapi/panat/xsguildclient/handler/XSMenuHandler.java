ce6edfad754b044c59787412fb91e426192073d8
XSectorZ <panatschool@gmail.com>
2024-02-15 14:40:06 +0700

    Init messages

---
+++ XSGuildClient/src/main/java/net/xsapi/panat/xsguildclient/handler/XSMenuHandler.java
@@ -0,0 +1,14 @@
+package net.xsapi.panat.xsguildclient.handler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class XSMenuHandler {
+
+    public static void openMainMenu(Player p) {
+
+        Inventory inv = Bukkit.createInventory(null,)
+    }
+
+}
